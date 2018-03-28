8139993bfcef3370e967662208f759c6907981e5
HO-JUNG AHN <labeldock@me.com>
2018-03-28 13:38:18 +0900

    nuxt + typescript (#1)
    * typescript config

--- template/pages/index.vue
+++ template/pages/index.vue
@@ -10,19 +10,14 @@
       </h2>
       <div class="links">
         <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+          @click="click"
+          class="button--green">Foo</a>
       </div>
     </div>
   </section>
 </template>
 
-<script>
+<script lang="ts">
 import AppLogo from '~/components/AppLogo.vue'
 
 export default {
